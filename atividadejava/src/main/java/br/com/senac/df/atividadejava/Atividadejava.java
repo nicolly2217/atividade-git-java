ca68fdc04280d4247a8beae9f89af856a7aaa0f4
nicolly2217 <nicollysiqueiraalves3@gmail.com>
2026-08-31 17:22:45 -0300

    projeto terminado

---
+++ atividadejava/src/main/java/br/com/senac/df/atividadejava/Atividadejava.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.senac.df.atividadejava;
+
+/**
+ *
+ * @author nicolly62977796
+ */
+public class Atividadejava {
+
+    public static void main(String[] args) {
+       int numero1 = 7;
+       int numero2 = 3;
+       int soma = numero1 + numero2;
+        
+        System.out.println("O resultado da soma é: " + soma);
+    }
+}
